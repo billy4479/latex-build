6719bcb5b06eb5defcb867862e2dde8d18a35dd1
billy4479 <giachi.ellero@gmail.com>
2025-01-05 18:31:27 +0100

    Add nix package

--- flake.nix
+++ flake.nix
@@ -14,8 +14,15 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        lib = pkgs.lib;
       in
       {
+        packages.default = pkgs.buildGoModule {
+          name = "latex-build";
+          src = lib.cleanSource ./.;
+          vendorHash = "sha256-hE34LiA/4x1+ncoduykEDacVqVPMQQd4Re0m13kGbEs=";
+        };
+
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             gopls
